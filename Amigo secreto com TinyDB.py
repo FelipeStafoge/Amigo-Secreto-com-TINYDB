0d82e198a7876bc07f14a5fb3855d5e9a8c9c08c
FelipeStafoge <felipestafoge11@gmail.com>
2023-01-07 17:01:27 -0300

    Try/Except adicionados. Bugs em "Apagar pessoas" corrigidos.

--- Amigo secreto com TinyDB.py
+++ Amigo secreto com TinyDB.py
@@ -6,10 +6,20 @@ db = TinyDB('db.json')
 pessoa = {}
 
 
+def linha():
+    print('-'*50)
+
+
 def menu():
     print('1- Ver Lista de Participantes\n2- Cadastrar Pessoas\n3- Apagar Pessoas\n4- Sorteio\n5- Sair')
     while True:
-        resp = int(input("Digite o que deseja fazer: "))
+        while True:
+            try:
+                resp = int(input("Digite o que deseja fazer: "))
+            except ValueError:
+                print('Digite apenas números!')
+            else:
+                break
         if 0 < resp <= 5:
             break
         else:
@@ -29,11 +39,13 @@ def menu():
 
 def lista():
     num = 0
-    print()
+    linha()
     for i in db.all():
         print(f"ID: {db.all()[num]['ID']}  Nome: {db.all()[num]['nome']} Presente: {db.all()[num]['presente']} | ", end='')
         num += 1
     print()
+    linha()
+
 
 
 def cadastro():
@@ -53,13 +65,30 @@ def cadastro():
 
 
 def remover():
+    listaid = []
+    soma = 0
+    for i in db.all():
+        listaid.append(db.all()[soma]['ID'])
+        soma += 1
     lista()
-    resp = int(input("Deseja apagar qual? <999 para cancelar e voltar pro menu!> "))
+    while True:
+        try:
+            resp = int(input("Escreva o ID que deseja apagar! <999 para voltar pro menu>: "))
+        except ValueError:
+            print('Digite apenas números!')
+        else:
+            break
     if resp == 999:
+        linha()
         menu()
+    if resp not in listaid:
+        print('ID inexistente! Digite um ID existente.')
+        linha()
+        remover()
     else:
         db.remove(doc_ids=[resp])
         print('Apagado com SUCESSO!')
+        linha()
         menu()
 
 
@@ -70,8 +99,8 @@ def sorteio():
         listaid.append(db.all()[soma]['ID'])
         soma += 1
     sorte = choice(listaid) - 1
-    print(listaid)
     print(f"O sorteado foi {db.all()[sorte]['nome']} e o que ele quer de presente é um/a {db.all()[sorte]['presente']}")
+    linha()
     menu()
 
 
